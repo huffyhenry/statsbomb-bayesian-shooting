0a599dc76b51d030e71535dce7f67ba9dbca2456
Marek Kwiatkowski <marek@mareklab.org>
2017-07-27 19:18:57 +0200

    Add/retouch graphs

--- graphs.R
+++ graphs.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(dplyr)
 
-make.top.plot <- function(ratings, topn=50){
+top.n.plot <- function(ratings, n=50){
   # Create a new player factor and sort it according to the skill estimate.
   ratings$player_name_aug <- sprintf("%s (%.0f%%)", ratings$player_name, 100*ratings$prob)
   ratings$player_name_aug <- factor(
@@ -10,12 +10,12 @@ make.top.plot <- function(ratings, topn=50){
   )
   ratings <- arrange(ratings, desc(mode))
 
-  ggplot(head(ratings, topn), aes(x=mode, y=player_name_aug)) +
+  ggplot(head(ratings, n), aes(x=mode, y=player_name_aug)) +
     geom_point(size=1.5) +
     geom_errorbarh(aes(xmin=low75, xmax=high75), size=0.25) +
     geom_vline(aes(xintercept=0), linetype='dotted') +
-    ggtitle(sprintf("The top %d finishers", topn)) +
-    labs(x="Player rating with 75% CI", y="Player name (P[skill > 0])") +
+    ggtitle(sprintf("The top %d finishers", n)) +
+    labs(x="Player rating w/ 75% CI", y="Player name w/ prob. of being above-average finisher") +
     theme(
       text=element_text(family='Palatino'),
       panel.grid.major.y=element_blank(),
@@ -23,3 +23,18 @@ make.top.plot <- function(ratings, topn=50){
       axis.ticks.y=element_blank()
     )
 }
+
+probabilities.plot <- function(ratings, top.n=40, bottom.n=10){
+  ratings <- arrange(ratings, desc(mode))
+  graph.data <- rbind(head(ratings, top.n), tail(ratings, bottom.n))
+  graph.data$player_name <- factor(
+    graph.data$player_name,
+    levels = graph.data$player_name[order(graph.data[, "mode"])]
+  )
+
+  ggplot(graph.data, aes(x=player_name, y=prob)) +
+    geom_bar(stat="identity") +
+    coord_flip()
+}
+
+
